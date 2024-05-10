7d17b624fd30e012c66c3430f4e07c0b402f5f36
Duong Thai Ngoc <thaingoctn5703@gmail.com>
2024-05-10 09:46:01 +0700

    project-2

---
+++ src/component/FirstComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Car extends React.Component {
+  render() {
+    return(
+        <div>
+            <h2>Hi, I am a Car!</h2>
+        </div>
+    );
+  }
+}
+
+export default Car;
+
+
