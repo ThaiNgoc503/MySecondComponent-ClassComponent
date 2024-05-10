9f23704b7dadd9c8aa664d4630b51dc0f3676abb
Duong Thai Ngoc <thaingoctn5703@gmail.com>
2024-05-10 13:23:48 +0700

    Project 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './App.css';
 import Car from './component/FirstComponent.js';
 
-function App() {
-  return (
-    <div>
-      <Car></Car>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div>
+//       <Car></Car>
+//     </div>
+//   );
+// }
+
+// chuyen thanh class coponent
+
+class App extends React.Component {
+  state = {
+  name : "Ngoc",
+  aga: 21,
+  yearCreateProject: 2024
+};
+  render(){
+    return (
+      <div>
+        <Car name = "Ngoc" ></Car>
+        my name is {this.state.name} and i'm {this.state.aga} year old and i create Project in: {this.state.yearCreateProject}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/component/FirstComponent.js
+++ src/component/FirstComponent.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+
+
 class Car extends React.Component {
+  constructor(props){
+    super(props); //goi ctst của lớp cha React.Component ko goi props se ko đc khởi tạo và giá trị == UNDIFIED
+    console.log(props);
+  }
   render() {
     return(
         <div>
-            <h2>Hi, I am a Car!</h2>
+              <h2>Hi, I am a Car! and i'm {this.props.name}</h2>
         </div>
     );
   }
